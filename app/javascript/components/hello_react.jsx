4596a3b2f0e895e67021eb2e10f1aef279a80978
Sam Ojling <sam.ojling@zenpayroll.com>
2019-02-14 18:19:01 -0800

    Restructured some of the js/components and fixed the ** ERROR: directory is already being watched! ** error

--- app/javascript/components/hello_react.jsx
+++ app/javascript/components/hello_react.jsx
@@ -3,10 +3,9 @@
 // of the page.
 
 import React from "react";
-import ReactDOM from "react-dom";
 import PropTypes from "prop-types";
 
-const Hello = props => <div>Hello {props.name}!!!</div>;
+const Hello = props => <div>Hello {props.name}!!!!!</div>;
 
 Hello.defaultProps = {
   name: "David"
@@ -16,9 +15,4 @@ Hello.propTypes = {
   name: PropTypes.string
 };
 
-document.addEventListener("DOMContentLoaded", () => {
-  ReactDOM.render(
-    <Hello name="React" />,
-    document.body.appendChild(document.createElement("div"))
-  );
-});
+export default Hello;
